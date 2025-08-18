e5487de1454f89e269cd7a1910df3eeef69afa2d
pixo2000 <firecraftpixo@gmail.com>
2025-08-18 15:11:15 +0200

    cleanup

--- tests.py
+++
@@ -1,7 +0,0 @@
-import aspose.slides as slides
-
-# Load presentation
-pres = slides.Presentation("Glasfaserabend_Niedernhausen Ost  & West.pptx")
-
-# Convert PPTX to PDF
-pres.save("pptx-to-pdf.pdf", slides.export.SaveFormat.PDF)
