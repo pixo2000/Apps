5bf64ebc655b966d079a98c298648edcb26b6e25
pixo2000 <firecraftpixo@gmail.com>
2025-08-28 09:44:15 +0200

    working video player

--- MNS-Studios/VideoSync/Client/mediaplayer.py
+++ MNS-Studios/VideoSync/Client/mediaplayer.py
@@ -1,79 +1,202 @@
-import os
-import cv2
-from ffpyplayer.player import MediaPlayer
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+from __future__ import annotations
 
-# Set the folder to use at the top
-MEDIA_FOLDER = os.path.abspath(r"C:/Users/Paul.Schoeneck.INFORMATIK/Downloads/SyncClient")  # CHANGE THIS
-PLAYLIST_FILE = os.path.join(MEDIA_FOLDER, "playlist.txt")
-VIDEOS_FOLDER = os.path.join(MEDIA_FOLDER, "Videos")
+"""PySide6 Multimedia player example"""
 
-# Read playlist
-with open(PLAYLIST_FILE, "r", encoding="utf-8") as f:
-    playlist = [line.strip() for line in f if line.strip()]
+import sys
+from PySide6.QtCore import QStandardPaths, Qt, Slot
+from PySide6.QtGui import QAction, QIcon, QKeySequence
+from PySide6.QtWidgets import (QApplication, QDialog, QFileDialog,
+                               QMainWindow, QSlider, QStyle, QToolBar)
+from PySide6.QtMultimedia import (QAudioOutput, QMediaFormat,
+                                  QMediaPlayer, QAudio)
+from PySide6.QtMultimediaWidgets import QVideoWidget
 
-def play_video_with_audio(video_path, window_created):
-    cap = cv2.VideoCapture(video_path)
-    player = MediaPlayer(video_path)
-    if not cap.isOpened():
-        print(f"[ERROR] Could not open video: {video_path}")
-        return
-    if not window_created[0]:
-        cv2.namedWindow("Media Player", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("Media Player", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        window_created[0] = True
-    delay = int(1000 / 28.5)  # 30 fps
-    while True:
-        grabbed, frame = cap.read()
-        audio_frame, val = player.get_frame()
-        if not grabbed:
-            break
-        if val != 'eof' and audio_frame is not None:
-            img, t = audio_frame
-        cv2.imshow("Media Player", frame)
-        key = cv2.waitKey(delay)
-        if key == 27:  # ESC
-            cap.release()
-            player.close_player()
-            cv2.destroyAllWindows()
-            os._exit(0)
-    cap.release()
-    player.close_player()
-    # Do not destroy window here
 
-def find_video_file(playlist_entry, folder_files):
-    entry_norm = playlist_entry.strip().lower()
-    for f in folder_files:
-        if f.strip().lower() == entry_norm:
-            return f
-    return None
+AVI = "video/x-msvideo"  # AVI
 
-if __name__ == "__main__":
-    print("[MEDIA FOLDER] Files in media folder:")
-    for f in os.listdir(MEDIA_FOLDER):
-        print(f"  {f}")
-    print("[TXT FILES] Contents of all .txt files in media folder:")
-    for fname in os.listdir(MEDIA_FOLDER):
-        if fname.lower().endswith('.txt'):
-            print(f"-- {fname} --")
-            with open(os.path.join(MEDIA_FOLDER, fname), "r", encoding="utf-8") as txtf:
-                for line in txtf:
-                    print(f"  {line.rstrip()}")
-    print("[PLAYLIST] Videos to play (in order):")
-    for idx, video in enumerate(playlist, 1):
-        print(f"  {idx}. '{video}'")
-    print("[FOLDER] Files in Videos folder:")
-    folder_files = os.listdir(VIDEOS_FOLDER)
-    for f in folder_files:
-        print(f"  {f}")
-    window_created = [False]
-    while True:
-        for video in playlist:
-            matched_file = find_video_file(video, folder_files)
-            if not matched_file:
-                print(f"[ERROR] No matching file for playlist entry: '{video}'")
-                continue
-            video_path = os.path.join(VIDEOS_FOLDER, matched_file)
-            print(f"[DEBUG] Playing: {video_path}")
-            print(f"[PLAYING] Now playing: {matched_file}")
-            play_video_with_audio(video_path, window_created)
-        # Repeat playlist
+
+MP4 = 'video/mp4'
+
+
+def get_supported_mime_types():
+    result = []
+    for f in QMediaFormat().supportedFileFormats(QMediaFormat.Decode):
+        mime_type = QMediaFormat(f).mimeType()
+        result.append(mime_type.name())
+    return result
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self._playlist = []  # FIXME 6.3: Replace by QMediaPlaylist?
+        self._playlist_index = -1
+        self._audio_output = QAudioOutput()
+        self._player = QMediaPlayer()
+        self._player.setAudioOutput(self._audio_output)
+
+        self._player.errorOccurred.connect(self._player_error)
+
+        tool_bar = QToolBar()
+        self.addToolBar(tool_bar)
+
+        file_menu = self.menuBar().addMenu("&File")
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.DocumentOpen)
+        open_action = QAction(icon, "&Open...", self,
+                              shortcut=QKeySequence.Open, triggered=self.open)
+        file_menu.addAction(open_action)
+        tool_bar.addAction(open_action)
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.ApplicationExit)
+        exit_action = QAction(icon, "E&xit", self,
+                              shortcut="Ctrl+Q", triggered=self.close)
+        file_menu.addAction(exit_action)
+
+        play_menu = self.menuBar().addMenu("&Play")
+        style = self.style()
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart,
+                               style.standardIcon(QStyle.SP_MediaPlay))
+        self._play_action = tool_bar.addAction(icon, "Play")
+        self._play_action.triggered.connect(self._player.play)
+        play_menu.addAction(self._play_action)
+
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.MediaSkipBackward,
+                               style.standardIcon(QStyle.SP_MediaSkipBackward))
+        self._previous_action = tool_bar.addAction(icon, "Previous")
+        self._previous_action.triggered.connect(self.previous_clicked)
+        play_menu.addAction(self._previous_action)
+
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackPause,
+                               style.standardIcon(QStyle.SP_MediaPause))
+        self._pause_action = tool_bar.addAction(icon, "Pause")
+        self._pause_action.triggered.connect(self._player.pause)
+        play_menu.addAction(self._pause_action)
+
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.MediaSkipForward,
+                               style.standardIcon(QStyle.SP_MediaSkipForward))
+        self._next_action = tool_bar.addAction(icon, "Next")
+        self._next_action.triggered.connect(self.next_clicked)
+        play_menu.addAction(self._next_action)
+
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStop,
+                               style.standardIcon(QStyle.SP_MediaStop))
+        self._stop_action = tool_bar.addAction(icon, "Stop")
+        self._stop_action.triggered.connect(self._ensure_stopped)
+        play_menu.addAction(self._stop_action)
+
+        self._volume_slider = QSlider()
+        self._volume_slider.setOrientation(Qt.Orientation.Horizontal)
+        self._volume_slider.setMinimum(0)
+        self._volume_slider.setMaximum(100)
+        available_width = self.screen().availableGeometry().width()
+        self._volume_slider.setFixedWidth(available_width / 10)
+        self._volume_slider.setValue(self._audio_output.volume() * 100)
+        self._volume_slider.setTickInterval(10)
+        self._volume_slider.setTickPosition(QSlider.TicksBelow)
+        self._volume_slider.setToolTip("Volume")
+        self._volume_slider.valueChanged.connect(self.setVolume)
+        tool_bar.addWidget(self._volume_slider)
+
+        icon = QIcon.fromTheme(QIcon.ThemeIcon.HelpAbout)
+        about_menu = self.menuBar().addMenu("&About")
+        about_qt_act = QAction(icon, "About &Qt", self, triggered=qApp.aboutQt)  # noqa: F821
+        about_menu.addAction(about_qt_act)
+
+        self._video_widget = QVideoWidget()
+        self.setCentralWidget(self._video_widget)
+        self._player.playbackStateChanged.connect(self.update_buttons)
+        self._player.setVideoOutput(self._video_widget)
+
+        self.update_buttons(self._player.playbackState())
+        self._mime_types = []
+
+    def closeEvent(self, event):
+        self._ensure_stopped()
+        event.accept()
+
+    @Slot()
+    def open(self):
+        self._ensure_stopped()
+        file_dialog = QFileDialog(self)
+
+        is_windows = sys.platform == 'win32'
+        if not self._mime_types:
+            self._mime_types = get_supported_mime_types()
+            if (is_windows and AVI not in self._mime_types):
+                self._mime_types.append(AVI)
+            elif MP4 not in self._mime_types:
+                self._mime_types.append(MP4)
+
+        file_dialog.setMimeTypeFilters(self._mime_types)
+
+        default_mimetype = AVI if is_windows else MP4
+        if default_mimetype in self._mime_types:
+            file_dialog.selectMimeTypeFilter(default_mimetype)
+
+        movies_location = QStandardPaths.writableLocation(QStandardPaths.MoviesLocation)
+        file_dialog.setDirectory(movies_location)
+        if file_dialog.exec() == QDialog.Accepted:
+            url = file_dialog.selectedUrls()[0]
+            self._playlist.append(url)
+            self._playlist_index = len(self._playlist) - 1
+            self._player.setSource(url)
+            self._player.play()
+
+    @Slot()
+    def _ensure_stopped(self):
+        if self._player.playbackState() != QMediaPlayer.StoppedState:
+            self._player.stop()
+
+    @Slot()
+    def previous_clicked(self):
+        # Go to previous track if we are within the first 5 seconds of playback
+        # Otherwise, seek to the beginning.
+        if self._player.position() <= 5000 and self._playlist_index > 0:
+            self._playlist_index -= 1
+            self._playlist.previous()
+            self._player.setSource(self._playlist[self._playlist_index])
+        else:
+            self._player.setPosition(0)
+
+    @Slot()
+    def next_clicked(self):
+        if self._playlist_index < len(self._playlist) - 1:
+            self._playlist_index += 1
+            self._player.setSource(self._playlist[self._playlist_index])
+
+    @Slot("QMediaPlayer::PlaybackState")
+    def update_buttons(self, state):
+        media_count = len(self._playlist)
+        self._play_action.setEnabled(media_count > 0 and state != QMediaPlayer.PlayingState)
+        self._pause_action.setEnabled(state == QMediaPlayer.PlayingState)
+        self._stop_action.setEnabled(state != QMediaPlayer.StoppedState)
+        self._previous_action.setEnabled(self._player.position() > 0)
+        self._next_action.setEnabled(media_count > 1)
+
+    def show_status_message(self, message):
+        self.statusBar().showMessage(message, 5000)
+
+    @Slot("QMediaPlayer::Error", str)
+    def _player_error(self, error, error_string):
+        print(error_string, file=sys.stderr)
+        self.show_status_message(error_string)
+
+    @Slot()
+    def setVolume(self):
+        self.volumeValue = QAudio.convertVolume(self._volume_slider.value() / 100.0,
+                                                QAudio.VolumeScale.LogarithmicVolumeScale,
+                                                QAudio.VolumeScale.LinearVolumeScale)
+        self._audio_output.setVolume(self.volumeValue)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    available_geometry = main_win.screen().availableGeometry()
+    main_win.resize(available_geometry.width() / 3,
+                    available_geometry.height() / 2)
+    main_win.show()
+    sys.exit(app.exec())
